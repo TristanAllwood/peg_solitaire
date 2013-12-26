55459a9e113b62d34cd9b719b59457a182fa2ae1
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2013-12-26 09:39:01 +0000

    Solid background, not tiled.

--- Pictures.hs
+++ Pictures.hs
@@ -52,7 +52,8 @@ renderBoard board (p, d)
                 , let tile = fromMaybe Blank (pieceAt position board)
                 ]
          | x <- [0..6]
-         ] # (if p /= (0,0) then attach "start" "end" else id)
+         ] # (if p /= (0,0) then attach "start" "end" else id) # centerX # centerY
+        `atop` square 14 # fc greenyellow # lc green
   where
     (jump, hole) = newPositions p d
 
@@ -62,7 +63,7 @@ attach n1 n2
     withName n2 $ \b2 ->
       atop (((location b1 ~~ location b2) # lc grey # lw 0.1) `atop` (circle 0.2 # fc grey # moveTo (location b2)))
 
-renderTile _ _ _ _ _ Blank = square 2 # fc black # lw 0
+renderTile _ _ _ _ _ Blank = tileBack
 renderTile d p jump hole position Hole  = (circle 0.75 # fc holeColour) `atop` links position `atop` tileBack
   where
     holeColour = black
@@ -74,12 +75,12 @@ renderTile d p jump hole position Peg   = (circle 0.75 # fc tileColour) `atop` l
       | jump == position = white
       | otherwise        = white
 
-tileBack = square 2 # fc greenyellow # lw 0
+tileBack = strutX 2 `atop` strutY 2 -- square 2 # fc greenyellow # lw 0
 
 links (x,y) = ((rotateBy (1/4) (line north ||| line south)) # centerY) `atop`
               ((line west  ||| line east) # centerX)
   where
-    line p = hrule 1.0 # (if p then lc black # lw 0.1 else lw 0.1 # lcA transparent)
+    line p = hrule 1.0 # (if p then (lc black # lw 0.1) else (lw 0.1 # lcA transparent))
 
     north = isPlayingLocation (x, y+1)
     east  = isPlayingLocation (x+1, y)
